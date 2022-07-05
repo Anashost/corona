3124cd09375102bcb8ce1f0c9344327ece1ef8b7
Anashost <41167157+Anashost@users.noreply.github.com>
2022-07-05 18:32:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ $.getJSON("https://disease.sh/v3/covid-19/countries/", function (data) {
 
         let html_code =
             '<tr>\n' +
-            '        <td>' <img class="flag-list" src="https://disease.sh/assets/img/flags/' + data[i].countryInfo.iso2.toLowerCase() + '.png">' + data[i].country + '</td>' +
+            '        <td><img class="flag-list" src="https://disease.sh/v3/covid-19/assets/img/flags/' + data[i].countryInfo.iso2.toLowerCase() + '.png">' + data[i].country + '</td>' +
             '        <td>' + data[i].cases + '<p id="plus-cases" class="set-red">+ ' + data[i].todayCases + '</p></td>\n' +
             '        <td>' + data[i].recovered + '</td>\n' +
             '        <td>' + data[i].deaths + '<p class="set-red">+ ' + data[i].todayDeaths + '</p></td>\n' +
